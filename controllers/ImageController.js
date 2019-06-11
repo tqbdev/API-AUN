a0b5b1fe66a2a5285b096dd4faab9302cb780f14
Bao Tran <tranquocbao3897@gmail.com>
2019-06-12 06:15:48 +0700

    [FEATURE] - Upload image from CKE5

---
+++ controllers/ImageController.js
@@ -0,0 +1,35 @@
+const _ = require('lodash');
+const path = require('path');
+const md5 = require('md5');
+const logger = require('log4js').getLogger('error');
+
+module.exports = {
+  async uploadImage(req, res) {
+    try {
+      const file = _.get(req, 'files.upload');
+
+      if (!file) {
+        return res.status(400).send({
+          error: 'No files were uploaded.'
+        });
+      }
+
+      const originName = _.get(file, 'name');
+      const hashName =
+        md5(originName + new Date().toDateString()) + path.extname(originName);
+      const appPath = path.normalize(__dirname + '/..');
+      const uploadPath = appPath + '/storage/' + hashName;
+      await file.mv(uploadPath);
+      const linkFile = '/storage/' + hashName;
+
+      res.send({
+        url: linkFile
+      });
+    } catch (err) {
+      logger.error(err);
+      res.status(500).send({
+        error: 'Error in upload image'
+      });
+    }
+  }
+};
