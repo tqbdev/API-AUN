e7eaf6181e24d557cce1e9e02b1af60dbe4ac677
Bao Tran <tranquocbao3897@gmail.com>
2019-06-26 15:24:20 +0700

    [FEATURE] - Add new engine to export pdf from html

--- utils/Extraction.js
+++ utils/Extraction.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const fs = require('fs');
 const path = require('path');
+const puppeteer = require('puppeteer');
 const md5 = require('md5');
 const htmlToPdf = require('html-pdf');
 
@@ -48,6 +49,38 @@ const createPdf = (html, fileName) => {
         else resolve(linkFile);
       });
   });
+};
+
+const createPdfNew = async (html, fileName) => {
+  const totalSeconds = Math.floor(new Date().getTime() / 1000);
+  const hashName = md5(fileName + totalSeconds) + '.pdf';
+  const appPath = path.normalize(__dirname + '/..');
+  const uploadPath = appPath + '/storage/' + hashName;
+  const linkFile = '/storage/' + hashName;
+
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  // await page.goto('https://blog.risingstack.com', {
+  //   waitUntil: 'networkidle0'
+  // });
+  await page.setContent(html);
+  const pdf = await page.pdf({
+    path: uploadPath,
+    format: 'A4',
+    displayHeaderFooter: true,
+    headerTemplate: `<div style="font-size: 10px; text-align: center; margin: 0 auto;">${fileName}<div/>`,
+    footerTemplate: `<div style="font-size: 10px; text-align: center; margin: 0 auto;">Page <span class='pageNumber'></span> / <span class='totalPages'></span></div>`,
+    margin: {
+      top: '100px',
+      bottom: '100px',
+      right: '100px',
+      left: '100px'
+    }
+  });
+
+  await browser.close();
+
+  return linkFile;
 };
 
 const extractReversionToPdf = async ReversionId => {
@@ -98,7 +131,8 @@ const extractReversionToPdf = async ReversionId => {
   content = _.replace(content, /strong>/g, 'b>');
 
   const finalHtml = _.replace(htmlTemplate, 'PLACE_TO_REPLACE', content);
-  return await createPdf(finalHtml, SAR.name);
+  // return await createPdf(finalHtml, SAR.name);
+  return await createPdfNew(finalHtml, SAR.name);
 };
 
 module.exports = {
